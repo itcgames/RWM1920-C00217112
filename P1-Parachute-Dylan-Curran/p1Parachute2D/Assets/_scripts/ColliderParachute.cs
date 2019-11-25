e43accb73a1e715c728faaacaca9ca5b5fb09f3b
Dylan Curran <c00217112@itcarlow.ie>
2019-11-25 14:50:06 +0000

    Add files via upload

---
+++ P1-Parachute-Dylan-Curran/p1Parachute2D/Assets/_scripts/ColliderParachute.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderParachute : MonoBehaviour
+{
+	[SerializeField] private GameObject parachute;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		parachute.SetActive(false);
+		gameObject.GetComponent<Rigidbody>().useGravity = true;
+	}
+}
---
+++ P1-Parachute-Dylan-Curran/p1Parachute2D/Assets/_scripts/GravityObjectScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityObjectScript : MonoBehaviour
+{
+	public GameObject objectFalling;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		objectFalling.GetComponent<Rigidbody>().useGravity = false;
+		gameObject.GetComponent<Rigidbody>().useGravity = false;
+
+		
+	}
+}
